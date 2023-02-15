14dd1d56cc8ab0ed6236bc30330b9b873e7afd57
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-02-15 17:30:08 -0300

    variáveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,1 +1,29 @@
+// o comando abaixo apresenta uma mensagem na tela
 console.log('Olá estou programando em javascript');
+
+/*
+     comentário que ocupa mais
+     de uma linha  
+*/
+
+// criação de variável do tipo texto
+let nome = 'Gabriel Amaro Pereira';
+// criação de variável do tipo numérico (inteiro)
+let idade = 35;
+// criação de variável do tipo numérico (decimal)
+let altura = 1.75;
+// criação de variável do tipo verdadeiro/falso
+let trabalha = true; // true = verdadeiro | false = falso
+// exemplo de variável de nome composto
+let corFavorita = 'Cinza';
+// exemplo de variável do tipo lista de valores
+let cores_favorista = ['Cinza', 'Preto', 'Azul'];
+// exemplo de variável que armazena um objeto
+let endereço = {'logradouro'      : 'Rua',
+                'nome_logradouro' : 'Brasil',
+                'número'          : '50 A',
+                'bairro'          : 'Centro',
+                'cidade'          : 'Tupã'
+};
+let covid;
+let dengue = null;
