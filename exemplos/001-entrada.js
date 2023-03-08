6f200eefc9f2f80e28bdf98ed1d71fb15049f0b0
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-03-08 17:19:48 -0300

    aula 08-03

---
+++ exemplos/001-entrada.js
@@ -0,0 +1,8 @@
+// Referência ppara a biblioteca prompt-sync
+let entrada = require ('prompt-sync')();
+
+// O prompt-sync permite capturar dados do teclado
+
+let nome = entrada ('Digite seu nome: ');
+console.log('O nome digitado foi: ' + nome);
+
