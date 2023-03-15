70bd82acdc573b9c531db386c9b85c02c39e2817
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-03-15 15:49:36 -0300

    Create 003-atividade.js

---
+++ exemplos/003-atividade.js
@@ -0,0 +1,8 @@
+// Crie um programa que receba seu nome e sua idade
+// e apresente na saída qual será sua idade daqui 
+// a 25 anos
+// Saída - ' Gabriel daqui a 25 anos a sua idade será 41
+
+let entrada = require('prompt-sync')();
+ console.log('Programa que recebe seu nome e sua idade');
+  
