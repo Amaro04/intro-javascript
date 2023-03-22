3cfa6cf655846a6e938126e32b1c73cad68e3d42
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-03-22 17:27:51 -0300

    aula 22/03

--- exemplos/005-atividade3.js
+++ exemplos/005-atividade3.js
@@ -7,4 +7,27 @@
    Geografia: 8,00
    Física: 8,00
    Média: 8,25
-*/   
+*/  
+const entrada = require('prompt-sync')();
+ console.log('Programa que recebe nome e nota de quatro matérias');
+
+  let matéria1 = entrada('Digite a primeira matéria: ');
+  let nota1 = parseFloat(entrada('Digite a nota: '));
+
+  let matéria2 = entrada('Digite a segunda matéria: ');
+  let nota2 = parseFloat(entrada('Digite a nota: '));
+
+  let matéria3 = entrada('Digite a terceira matéria: ');
+  let nota3 = parseFloat(entrada('Digite a nota: '));
+
+  let matéria4 = (entrada('Digite a quarta matéria: '));
+  let nota4 = parseFloat(entrada('Digite a nota: ')); 
+
+let média = (nota1 + nota2 + nota3 + nota4) /4;
+
+console.log('Notas: ');
+console.log(matéria1 + ': ' + nota1.toFixed(2));
+console.log(matéria2 + ': ' + nota2.toFixed(2));
+console.log(matéria3 + ': ' + nota3.toFixed(2));
+console.log(matéria4 + ': ' + nota4.toFixed(2));
+console.log('Média: ' + média.toFixed(2));
