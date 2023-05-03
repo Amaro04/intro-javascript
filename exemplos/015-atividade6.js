ed1cd0ffb456fc430677a57139814e00c97073d8
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-05-03 17:28:49 -0300

    03-05
    slv

--- exemplos/015-atividade6.js
+++ exemplos/015-atividade6.js
@@ -4,3 +4,27 @@ exiba a mensagem "Bem vindo" caso o usuário
 digte a senha corretamente e "Usuario bloqueado"
 caso o usuário erre a senha 3 vezes!
 */ 
+
+const entrada = require('prompt-sync')();
+
+const senha = '40028922';
+let senhaDigitada = '';
+let erros = -1;
+
+do{
+    erros++;
+
+    if (erros >= 3){
+        break;
+    }
+   senhaDigitada = entrada('Digite sua senha para entrar: ');
+}
+while (senha != senhaDigitada);
+
+if (erros <3){
+    console.log('Bem-Vindo!');
+}
+else{
+    console.log('Você errou a senha 3 vezes, seu acesso foi negado!') 
+}
+
---
+++ exemplos/016-array.js
@@ -0,0 +1,21 @@
+// Array
+
+/*
+O que não fazer:
+
+let aluno1 = 'Teo Trajano';
+let aluno2 = 'Raíssa Rebeca';
+let aluno3 = 'Carlos Caetano';
+let aluno4 = 'Helena Harris';
+
+console.log('Lista de alunos:');
+console.log('- ' + aluno1);
+console.log('- ' + aluno2);
+console.log('- ' + aluno3);
+console.log('- ' + aluno4);
+*/
+
+//O que fazer:
+
+let alunos = ['Teo Trajano', 'Raíssa Rebeca', 'Carlos Caetano','Helena Harris'];
+console.log(alunos);
