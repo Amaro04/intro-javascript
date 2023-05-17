b28077d6f82b5acb7e3b9fc478877ff83bba5168
Amaro04 <124818775+Amaro04@users.noreply.github.com>
2023-05-17 17:27:34 -0300

    aula 17-05-23

---
+++ exemplos/019-mega_deverdade.js
@@ -0,0 +1,42 @@
+/*
+Programa da mega-sena
+Crie um programa que compare o resultado de dois arrays
+*/
+
+const entrada = require('prompt-sync')();
+
+let resMegaSena = [];
+let meuJogo = [];
+let acertos = 0;
+
+for (let i = 0; i < 6; i++){
+    const numero = Math.floor(Math.random() * 60) + 1;
+    resMegaSena.push(numero);
+}
+
+for (let i = 1; i < 6; i++) {
+    let numero = entrada ('Digite o ' + i + 'º nº do jogo: ');
+    meuJogo.push(numero);
+}
+
+for (let nMega = 0; nMega < resMegaSena.length; nMega++) {
+    for (let nJogo = 0; nJogo < meuJogo.length; nJogo++) {
+      if (resMegaSena[nMega] === meuJogo[nJogo]) {
+          acertos++;
+      }
+    }
+}
+
+let mensagem = 'Resultado do sorteio: ';
+
+for (let n = 0; n < resMegaSena.length; n++){
+     mensagem = mensagem + resMegaSena[n] + (resMegaSena.length == 5) ? '' : ' - '; 
+}
+
+mensagem = 'Seu Jogo: ';
+
+for (let n = 0; n < meuJogo.length; n++){
+     mensagem = mensagem + meuJogo[n] + (meuJogo.length == 5) ? '' : ' - '; 
+}
+
+console.log('Você acertou ' + acertos + ' número' + (acertos > 1 ? 's' : ''));
